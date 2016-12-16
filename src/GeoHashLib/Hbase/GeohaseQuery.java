a01239a240d8a829ea444620f579160b0f95e65f
coco11563 <coco11563@yeah.net>
2016-12-16 14:31:42 +0800

    升级了GUAVA20的jar包

---
+++ src/GeoHashLib/Hbase/GeohaseQuery.java
@@ -0,0 +1,12 @@
+package GeoHashLib.Hbase;
+
+import java.util.List;
+
+/**
+ * Created by root on 12/16/16.
+ */
+public class GeohaseQuery {
+    public List<String> queryKnn(double lat, double lon, int n) {
+
+    }
+}
